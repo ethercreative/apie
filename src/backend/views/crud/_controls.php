302633bebeec3b2dc63c8916c0a9f045467ad606
Matt Edmonston <matt@ethercreative.co.uk>
2018-01-15 15:54:52 +0000

    Checking model has archive property.

--- src/backend/views/crud/_controls.php
+++ src/backend/views/crud/_controls.php
@@ -7,5 +7,5 @@ use yii\helpers\Html;
 <div class="form-group">
     <?= Html::submitButton('<i class="glyphicon glyphicon-floppy-disk"></i> Save', ['class' => 'btn btn-primary']) ?>
     <?= $model->isNewRecord ? Html::submitButton('<i class="glyphicon glyphicon-floppy-disk"></i> Save and add another', ['class' => 'btn btn-primary', 'name' => 'additional', 'value' => 'yes']) : null ?>
-    <?= !$model->isNewRecord ? Html::a('<i class="glyphicon glyphicon-compressed"></i> Archive', ['archive', 'id' => $model->id], ['class' => 'btn btn-danger pull-right']) : null ?>
+    <?= !$model->isNewRecord && in_array('archived', array_keys($model::getTableSchema()->columns)) ? Html::a('<i class="glyphicon glyphicon-compressed"></i> Archive', ['archive', 'id' => $model->id], ['class' => 'btn btn-danger pull-right']) : null ?>
 </div>
